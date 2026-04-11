b0ed462df469a1eb0aa87087cff6eed6fbd73390
Rmd2015 <ibad.ram1@gmail.com>
2026-04-11 15:45:46 +0100

    modifier l endpoint create

---
+++ Backend/Controllers/ChatRoomController.cs
@@ -0,0 +1,83 @@
+﻿using Backend.Data;
+using Backend.Models.DTOs;
+using Backend.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatRoomController : Controller
+    {
+        private readonly ChatDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public ChatRoomController(ChatDbContext context ,IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // ====================== PUBLIC ROUTES ======================
+
+
+
+        // ====================== PROTECTED ROUTES ======================
+        [HttpPost("Create")]
+        [Authorize]
+        public async Task<ActionResult<ChatroomDto>> Create([FromBody] CreateChatroomDto dto)
+        {
+            // 1. Validation du modèle
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                var Owner = await _context.Chatuser.FirstOrDefaultAsync(u => u.Iduser == dto.CreatedByUserId);
+                if (Owner == null)
+                {
+                    return BadRequest(new
+                    {
+                        message = "User non trouvé!"
+                    });
+                }
+               
+                var NewChatRoom = new Chatroom
+                {
+                    Iduser = dto.CreatedByUserId,
+                    Roomcreatedat = DateTime.UtcNow,
+                    Chatroomlabel = dto.ChatRoomLabel,
+
+                };
+                await _context.Chatroom.AddAsync(NewChatRoom);
+                await _context.SaveChangesAsync();
+                //creation de new chatroommember  !! 
+                var NewChatRoomMember = new Chatroommember
+                {
+                    Iduser = dto.CreatedByUserId,
+                    Idchatroom = NewChatRoom.Idchatroom,
+                    Useraddat = DateTime.UtcNow,
+
+                };
+                await _context.Chatroommember.AddAsync(NewChatRoomMember);
+                await _context.SaveChangesAsync();
+                return Ok(new
+                {
+                    success = true,
+                    message = "Chat Romm Ajouter avec succès."
+                });
+            }
+            catch (Exception ex) { 
+            return BadRequest(new
+            {
+               details =  ex.Message,
+               message = "Probleme c`est produit!",
+
+
+            });
+            }
+        }
+      
+    }
+}
